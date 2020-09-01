123556228ff69b9ea16b6f6ba31b69eba31562cd
slasher2010 <you@example.com>
2020-09-02 02:32:24 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io()
 
 socket.on('connect', function () {
 	console.log('Connected to the server')
-
-	socket.emit('createMessage', {
-		from: 'Mauro',
-		text: 'Yup that works for me'
-	})
 })
 
 socket.on('disconnect', function () {
