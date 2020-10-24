c6b8e83d3d6cd067cd6e212246c938839d4d5471
slasher2010 <you@example.com>
2020-10-25 00:05:59 +0800

    Add users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,46 @@
+// class Person {
+// 	constructor(name, age) {
+// 		this.name = name
+// 		this.age = age
+// 	}
+// }
+
+// var me = new Person('Mauro', 27)
+
+// console.log('this.name', me.name)
+// console.log('this.age', me.age)
+
+class Users {
+	constructor() {
+		this.users = []
+	}
+
+	addUser (id, name, room) {
+		var user = {id, name, room}
+		this.users.push(user)
+		return user
+	}
+
+	removeUser (id) {
+		var user = this.getUser(id)
+
+		if (user) {
+			this.users = this.users.filter((user) => user.id !== id)
+		}
+
+		return user
+	}
+
+	getUser (id) {
+		return this.users.filter((user) => user.id === id)[0]
+	}
+
+	getUserList (room) {
+		var users = this.users.filter((user) => user.room === room)
+		var nameArray = users.map((user) => user.name)
+
+		return nameArray
+	}
+}
+
+module.exports = {Users}
