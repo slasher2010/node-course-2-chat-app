d2aa69bc6fd3fe338314680f7f6726b300e76971
slasher2010 <you@example.com>
2020-10-25 00:24:01 +0800

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -71,8 +71,7 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e){
 	e.preventDefault()
 	var messageTextbox = jQuery('[name=message]')
-	socket.emit('createMessage',{
-		from: 'User',
+	socket.emit('createMessage', {
 		text: messageTextbox.val()
 	}, function () {
 		messageTextbox.val('')
